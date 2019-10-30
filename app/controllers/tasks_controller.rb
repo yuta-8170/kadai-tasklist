9ccccc005053fd88aa1355103a0405f074e36014
yuta.yamamoto <1mg6040y@komazawa-u.ac.jp>
2019-10-30 14:54:28 +0000

    first commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,10 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-      @tasks = Task.all
+      if logged_in?
+        @tasks = current_user.tasks
+      end
   end
 
   def show
@@ -12,7 +15,7 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
       
       if @task.save
           flash[:success] = 'Taskが正常に投稿されました'
